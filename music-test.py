7e67d5fa0a68da01fa92f515a1acdf8a6dca397a
scottsuk0306 <scottsuk0306@kaist.ac.kr>
2022-06-06 03:45:36 +0900

    Create music-test.py

---
+++ music-test.py
@@ -0,0 +1,8 @@
+from playsound import playsound
+
+# Path has to be in absolute path!
+# On Windows
+playsound("C:\\Users\\Scott Suk\\OneDrive\\문서\\GitHub\\smart-barbell\\sample-15s.mp3")
+
+# On Linux - Fails
+# playsound("C:\Users\Scott Suk\OneDrive\문서\GitHub\smart-barbell\sample-15s.mp3")
